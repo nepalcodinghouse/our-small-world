6e80e0d876ddad90a8ec38fd7fed33d6339aa91c
nepalcodinghouse <nepalcodinghouse@gmail.com>
2025-12-06 11:49:52 +0545

    added comming-s00n

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route , Router} from 'react-router-dom'
+import Home from './pages/Home'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ComingSoon() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white flex-col gap-6">
+      <h1 className="text-5xl font-bold">We are Coming Soon</h1>
+      <p className="text-gray-400">Stay tuned for something amazing!</p>
+    </div>
+  );
+}
